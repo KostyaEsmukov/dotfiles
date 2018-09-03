1958ad63f6b74ea7c0e28af7a94c950d3a850fba
Kostya Esmukov <kostya@esmukov.ru>
2018-09-03 16:40:08 -0400

    Update GNOME extensions

--- scripts/pullup_gnome.sh
+++ scripts/pullup_gnome.sh
@@ -26,10 +26,13 @@ mkdir -p ~/.local/share/gnome-shell/extensions
 echo "Opening Chromium window with the Gnome extensions to install..."
 chromium-browser \
     https://extensions.gnome.org/extension/2/move-clock/ \
-    https://extensions.gnome.org/extension/28/gtile/ \
+    https://extensions.gnome.org/extension/258/notifications-alert-on-user-menu/ \
     https://extensions.gnome.org/extension/307/dash-to-dock/ \
     https://extensions.gnome.org/extension/495/topicons/ \
+    https://extensions.gnome.org/extension/708/panel-osd/ \
     https://extensions.gnome.org/extension/723/pixel-saver/ \
     https://extensions.gnome.org/extension/1037/customcorner/ \
+    https://extensions.gnome.org/extension/1236/noannoyance/ \
+    https://extensions.gnome.org/extension/1253/extended-gestures/ \
     ''
 
