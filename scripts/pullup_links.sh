0b109450057f7ca41c7c46cd658631b68706b468
Kostya Esmukov <kostya@esmukov.ru>
2018-12-14 23:15:05 +0300

    Remove .ideavimrc

--- scripts/pullup_links.sh
+++ scripts/pullup_links.sh
@@ -8,12 +8,12 @@
 # - gitconfig is linked by install_git_ssh.sh
 
 ln -svf ~/dotfiles/ackrc ~/.ackrc
-ln -svf ~/dotfiles/ideavimrc ~/.ideavimrc
 ln -svf ~/dotfiles/flake8 ~/.config/flake8
 ln -svf ~/dotfiles/isort.cfg ~/.isort.cfg
 ln -svf ~/dotfiles/gitignore_global ~/.gitignore_global
 ln -svf ~/dotfiles/tmux.conf ~/.tmux.conf
 
 # Cleanup previously existing links
+[ -L ~/.ideavimrc ] && rm -vf ~/.ideavimrc
 [ -L ~/.tigrc ] && rm -vf ~/.tigrc
 
