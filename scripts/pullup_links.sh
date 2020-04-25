391eec93231b3c179e07ce2dc02a485b90bc2586
Kostya Esmukov <kostya@esmukov.ru>
2020-04-25 15:51:47 +0300

    pullup_links: fix error when .config dir is missing

--- scripts/pullup_links.sh
+++ scripts/pullup_links.sh
@@ -7,6 +7,8 @@
 # - nvim/init.vim is linked by pullup_nvim.sh
 # - gitconfig is linked by install_git_ssh.sh
 
+mkdir -p ~/.config
+
 ln -svf ~/dotfiles/ackrc ~/.ackrc
 ln -svf ~/dotfiles/flake8 ~/.config/flake8
 ln -svf ~/dotfiles/gitignore_global ~/.gitignore_global
