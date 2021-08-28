8370ee0b12cbad10701ff5d531e77a5c8f3691f7
Kostya Esmukov <kostya@esmukov.ru>
2021-08-29 01:48:57 +0300

    Remove unused ackrc config

--- scripts/pullup_links.sh
+++ scripts/pullup_links.sh
@@ -9,13 +9,13 @@
 
 mkdir -p ~/.config
 
-ln -svf ~/dotfiles/ackrc ~/.ackrc
 ln -svf ~/dotfiles/flake8 ~/.config/flake8
 ln -svf ~/dotfiles/gitignore_global ~/.gitignore_global
 ln -svf ~/dotfiles/isort.cfg ~/.isort.cfg
 ln -svf ~/dotfiles/tmux.conf ~/.tmux.conf
 
 # Cleanup previously existing links
+[ -L ~/.ackrc ] && rm -vf ~/.ackrc
 [ -L ~/.ideavimrc ] && rm -vf ~/.ideavimrc
 [ -L ~/.tigrc ] && rm -vf ~/.tigrc
 
