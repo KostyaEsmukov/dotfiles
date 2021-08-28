2af26c813aefe07f65758e4f034f541e964db950
Kostya Esmukov <kostya@esmukov.ru>
2021-08-28 14:26:00 +0300

    install_apps: add spotify, firefox, telegram desktop, zoom, vlc

--- scripts/install_apps.sh
+++ scripts/install_apps.sh
@@ -2,11 +2,13 @@
 
 set -euxo pipefail
 
-# TODO (?) telegram, firefox dev ed, redis
-
 if [ "Darwin" = `uname` ]; then
 
+    open "https://www.spotify.com/download/mac/"
+
+    open "https://www.mozilla.org/en-US/firefox/mac/"
     open "https://www.google.com/chrome/"
+
     open "https://www.keepassx.org/downloads"
     open "https://www.iterm2.com/"
     open "https://git-fork.com/"
@@ -17,6 +19,9 @@ if [ "Darwin" = `uname` ]; then
 
     open "https://www.virtualbox.org/wiki/Downloads"
 
+    open "https://desktop.telegram.org/"
+    open "https://zoom.us/download"
+
     # -- macos-specific
 
     open "https://www.keka.io/"
@@ -24,6 +29,7 @@ if [ "Darwin" = `uname` ]; then
 
     brew install --cask \
         kap \
+        vlc \
         wireshark \
     ;
 
