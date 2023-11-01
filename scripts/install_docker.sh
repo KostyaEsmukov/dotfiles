53c809bfe3e8e1d98594e54173e36679bb9c2a6a
Kostya Esmukov <kostya@esmukov.net>
2023-11-01 10:58:48 +0200

    docker on debian: switch to docker.io

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -8,36 +8,8 @@ if [ "Darwin" = `uname` ]; then
 
 elif which apt; then
 
-    # https://docs.docker.com/install/linux/docker-ce/debian/
-
-    if [ "x86_64" != `uname -m` ]; then
-        echo "This script is for x86_64 only."
-        echo "Please install docker manually instead."
-        exit 1
-    fi
-
-    if [ "Debian" != `lsb_release -si` ]; then
-        echo "This script is for Debian."
-        echo "Please install docker manually instead."
-        exit 1
-    fi
-
-    sudo apt install \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg2 \
-        software-properties-common
-
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-
-    sudo add-apt-repository \
-        "deb [arch=amd64] https://download.docker.com/linux/debian \
-        $(lsb_release -cs) \
-        stable"
-
     sudo apt update
-    sudo apt install docker-ce
+    sudo apt install --no-install-recommends docker.io docker-compose
 
     sudo gpasswd -a `whoami` docker
 else
